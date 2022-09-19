392927f0779033085c090691cca1e975036821ad
Yuri Mel0 <meloy316@gmail.com>
2022-09-18 23:14:01 -0300

    Exemplo002 quase finalizado, ainda falta organizar os inputs por ponteiros e deixar tudo mais dinamico
     Changes to be committed:
    	modified:   Exemplo002/Exemplo002.depend
    	modified:   Exemplo002/Exemplo002.layout
    	modified:   Exemplo002/bin/Debug/Exemplo002.exe
    	modified:   Exemplo002/main.cpp
    	modified:   Exemplo002/obj/Debug/main.o

--- Exemplo002/main.cpp
+++ Exemplo002/main.cpp
@@ -10,6 +10,8 @@
  *
 */
 #include <iostream>
+#define A 5
+
 
 using namespace std;
 
@@ -30,11 +32,9 @@ struct t_Aluno
 
     void Print(void)
     {
-        cout << " Registro de Aluno " << endl;
-        cout << " DRE : " << DRE << endl;
         cout << " Nome: " << Nome << endl;
-        cout << " CR  : " << CR << endl;
-        cout << "                       " << endl;
+        cout << " DRE : " << DRE << endl;
+        cout << " CR  : " << CR << "\n\n";
     }
 
     double operator +(t_Aluno B)
@@ -49,20 +49,33 @@ class t_Curso
  public:
     string  Codigo_Curso;
     string  Nome_Curso;
-    t_Aluno lista_de_alunos[30];
+    t_Aluno *Lista_de_alunos = new t_Aluno[A];
+    double Nota_mec;
 
-    void Cadastro(string pCodigo_Curso, string pNome_Curso)
+    void Cadastro(string pCodigo_Curso, string pNome_Curso, double pNota_mec)
     {
         Codigo_Curso = pCodigo_Curso;
         Nome_Curso = pNome_Curso;
+        Nota_mec = pNota_mec;
+
     }
 
     void Print(void)
     {
-        cout << " Registro do Curso " << endl;
+        cout << "\n" << " Registro do Curso " << endl;
         cout << " Codigo Curso : " << Codigo_Curso << endl;
         cout << " Nome Curso: " << Nome_Curso << endl;
-        cout << " Alunos : " << endl;
+        cout << " Nota MEC: " << Nota_mec << endl;
+        cout << " Alunos : " << "\n\n";
+              Lista_de_alunos[0].Print();
+              Lista_de_alunos[1].Print();
+              Lista_de_alunos[2].Print();
+              Lista_de_alunos[3].Print();
+              Lista_de_alunos[4].Print();
+    }
+    double operator +(t_Curso B)
+    {
+        return (Nota_mec + B.Nota_mec);
     }
 };
 
@@ -76,18 +89,25 @@ double Media(T a1, T a2)
 
 int main()
 {
-
     cout << "+-------------------------------------------- +" << endl;
     cout << "|         Exemplo 002: Aluno e Curso          |" << endl;
     cout << "+-------------------------------------------- +" << endl;
 
-    t_Aluno a1;
-    a1.Cadastro(12222222222, "fic", 4.5);
-    a1.Print();
+    t_Aluno a1, a2, a3, a4, a5;
+    a1.Cadastro(12222222222, "Anonimo1", 4.7);
+    a2.Cadastro(13333333333, "Anonimo2", 4.8);
+    a3.Cadastro(14444444444, "Anonimo3", 4.9);
+    a4.Cadastro(15555555555, "Anonimo4", 5.0);
+    a5.Cadastro(16666666666, "Anonimo5", 5.1);
 
     t_Curso c1;
-    c1.Cadastro("MAE015", "Estrutura de dados");
+    c1.Cadastro("BCMT01", "BCMT", 4.5);
+    c1.Lista_de_alunos[0] = a1;
+    c1.Lista_de_alunos[1] = a2;
+    c1.Lista_de_alunos[2] = a3;
+    c1.Lista_de_alunos[3] = a4;
+    c1.Lista_de_alunos[4] = a5;
     c1.Print();
 
-    return 0;
+   return 0;
 }
